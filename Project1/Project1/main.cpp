3b5afa9c12ff477f23a0dac9ccff85583b7b1d02
Tokusige-0411 <52820074+Tokusige-0411@users.noreply.github.com>
2021-05-21 11:47:14 +0900

    hennkou

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,10 +1,13 @@
-#include<Dxlib.h>
+#include<DxLib.h>
+#include<string>
+#include<sstream>
+#include<iomanip>
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
 	SetGraphMode(1280, 720, 32);
 	ChangeWindowMode(true);
-	SetWindowText(L"1916027_ìøèdå’ëÂòN");
+	SetWindowText("1916027_ìøèdå’ëÂòN");
 
 	if (DxLib_Init() == -1)
 	{
@@ -12,9 +15,41 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	}
 
 	SetDrawScreen(DX_SCREEN_BACK);
-	while (ProcessMessage() == 0 )
+	int x = 100;
+	int y = 100;
+
+	//int gHandle = LoadGraph("Asset/Adventurer-1.5/Individual Sprites/adventurer-run-00.png");
+	int gHandle[6] = {};
+
+	for (int i = 0; i < 6; i++)
 	{
+
+	}
+
+	while (ProcessMessage() == 0)
+	{
+		if (CheckHitKey(KEY_INPUT_UP))
+		{
+			y -= 5;
+		}
+		if (CheckHitKey(KEY_INPUT_DOWN))
+		{
+			y += 5;
+		}
+		if (CheckHitKey(KEY_INPUT_RIGHT))
+		{
+			x += 5;
+		}
+		if (CheckHitKey(KEY_INPUT_LEFT))
+		{
+			x -= 5;
+		}
+
 		ClsDrawScreen();
+
+		DrawBox(x, y, x + 100, y + 100, 0xffffff, false);
+		DrawString(610, 355, "Hello, World!", 0xffffff);
+		DrawRotaGraph(x, y, 4.0, 0.0, gHandle, true, false);
 
 		ScreenFlip();
 	}
